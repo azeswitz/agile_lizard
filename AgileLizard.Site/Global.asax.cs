5c84d2dc82eee374acdfbf3e47a7b26627b62200
azeswitz <AZeswitz@icf2001008.icfi.icfconsulting.com>
2015-01-20 12:21:30 -0500

    Debugging deployment configuration
    Modified log file name;
    Added global error logging;

--- AgileLizard.Site/Global.asax.cs
+++ AgileLizard.Site/Global.asax.cs
@@ -6,11 +6,14 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Http;
+using log4net;
+using System.Reflection;
 
 namespace AgileLizard.Site
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         protected void Application_Start()
         {
 
@@ -22,6 +25,18 @@ namespace AgileLizard.Site
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AutoMapperConfig.Init();
+        }
+
+        protected void Application_Error()
+        {
+            Exception exception = Server.GetLastError();
+            HttpException httpException = exception as HttpException;
+
+            var code = httpException == null ? 500 : httpException.GetHttpCode();
+            // Log the exception.
+            log.Error(exception);
+
+            Server.ClearError();
         }
     }
 }
